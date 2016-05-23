51a10af082252e63e2d2786e6445b52cca754e7e
kawashinshin <kawakamis@kawakamishintarou-no-MacBook-Pro.local>
2016-05-23 15:38:46 +0900

    ツイートの編集機能を追加 １．ツイート編集画面のルーティングを設定する ２．編集ボタンをビューに追加する ３．editアクションをコントローラーに定義する ４．編集画面のビューファイルを作成する ５．ツイートの更新を行なうためのルーティングを設定する ６．updateアクションをコントローラーに定義する ７．更新後のビューファイルを追加する

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,12 +4,12 @@
 
     def index
         @tweets = Tweet.includes(:user).order("created_at DESC").page(params[:page]).per(5)
-  end
+    end
 
     def new
     end
 
-  def create
+    def create
       Tweet.create(image: tweet_params[:image], text: tweet_params[:text], user_id: current_user.id)
     end
 
@@ -17,15 +17,26 @@
       tweet = Tweet.find(params[:id])
       if tweet.user_id == current_user.id
         tweet.destroy
+      end
     end
-  
+
+    def edit
+      @tweet = Tweet.find(params[:id])
     end
+
+    def update
+      tweet = Tweet.find(params[:id])
+      if tweet.user_id == current_user.id
+      tweet.update(tweet_params)
+      end
+    end
+
     private
     def tweet_params
       params.permit(:image, :text)
     end
 
-    def move_to_index
-      redirect_to action: :index unless user_signed_in?
+     def move_to_index
+      redirect_to :action => "index" unless user_signed_in?
     end
   end
