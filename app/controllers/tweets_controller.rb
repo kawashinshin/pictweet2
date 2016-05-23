7ae021afa602e83be72a72ff9eb78f4999fb6c03
kawashinshin <kawakamis@kawakamishintarou-no-MacBook-Pro.local>
2016-05-23 12:27:52 +0900

    ログイン作成。ログインをせずに投稿しようとすると、indexに戻る仕様。

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
   class TweetsController < ApplicationController
 
+    before_action :move_to_index, except: :index
+
     def index
         @tweets = Tweet.order("created_at DESC").page(params[:page]).per(5)
   end
@@ -16,4 +18,7 @@
       params.permit(:name, :image, :text)
     end
 
+    def move_to_index
+      redirect_to action: :index unless user_signed_in?
+    end
   end
