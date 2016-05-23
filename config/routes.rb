ef04db6566e0fa378dfa5922462bd3a029e3d2ce
kawashinshin <kawakamis@kawakamishintarou-no-MacBook-Pro.local>
2016-05-23 14:48:59 +0900

    ツイートの削除機能を追加 １．ツイートを削除するためのルーティングを設定する ２．削除ボタンをビューに追加する ３．destroyアクションをコントローラーに定義する ４．削除後のビューを追加する

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
-  Rails.application.routes.draw do
-  devise_for :users
-    root 'tweets#index'
-    get   'tweets'      =>  'tweets#index'     #ツイート一覧画面
-    get   'tweets/new'  =>  'tweets#new'       #ツイート投稿画面
-    post  'tweets'      =>  'tweets#create'    #ツイート投稿機能
-    get   'users/:id'   =>  'users#show'
+Rails.application.routes.draw do
+    devise_for :users
+    root  'tweets#index'
+    get   'tweets'      =>  'tweets#index'
+    get   'tweets/new'  =>  'tweets#new'
+    post  'tweets'      =>  'tweets#create'
+    delete  'tweets/:id'  => 'tweets#destroy'
+    get   'users/:id'  =>  'users#show'
   end
